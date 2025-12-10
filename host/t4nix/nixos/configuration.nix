b0f528c78524940223d887adcb12a22f38966f4a
mirzaganteng <dev@mrz.my.id>
2025-12-10 23:55:41 +0700

    feat: use nix community cache

--- host/t4nix/nixos/configuration.nix
+++ host/t4nix/nixos/configuration.nix
@@ -16,7 +16,18 @@
     };
   };
 
-  nix.settings = {experimental-features = toString ["nix-command" "flakes"];};
+  nix.settings = {
+    experimental-features = toString ["nix-command" "flakes"];
+    trusted-users = ["ryan"];
+
+    # see https://nix-community.org/cache/
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   time.timeZone = "Asia/Jakarta";
   i18n.defaultLocale = "en_US.UTF-8";
