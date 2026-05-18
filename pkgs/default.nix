22d2002f59cb4a5e80fbce166b772beea96729ba
mirzaganteng <dev@mrz.my.id>
2026-05-18 09:22:59 +0700

    chore: sort pkgs definition

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,7 @@
 }: {
   neovim = inputs.nvim.packages.${final.stdenv.hostPlatform.system}.default;
   neovim-static = inputs.nvim.packages.${final.stdenv.hostPlatform.system}.neovim-static;
-  ssh-sign-me-up = final.writeShellScriptBin "ssh-sign-me-up" (builtins.readFile ./ssh-sign-me-up.sh);
   open-github = final.writeShellScriptBin "open-github" (builtins.readFile ./open-github.sh);
   release-please = final.callPackage ./release-please.nix {};
+  ssh-sign-me-up = final.writeShellScriptBin "ssh-sign-me-up" (builtins.readFile ./ssh-sign-me-up.sh);
 }
