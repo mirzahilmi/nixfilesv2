1a7622369a4b2b7bb0d54d3af0eb082f2fe85e79
Mirza Hilmi <mirza.hilmi@evidence.care>
2026-07-13 07:14:30 +0000

    build(carefull): override lazygit to v0.63.0

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,4 +9,18 @@
   release-please = final.callPackage ./release-please.nix {};
   ssh-sign-me-up = final.writeShellScriptBin "ssh-sign-me-up" (builtins.readFile ./ssh-sign-me-up.sh);
   xdg-open = final.writeShellScriptBin "xdg-open" (builtins.readFile ./xdg-open.sh);
+
+  lazygit = prev.lazygit.overrideAttrs (old: rec {
+    version = "0.63.0";
+    src = prev.fetchFromGitHub {
+      owner = "jesseduffield";
+      repo = "lazygit";
+      tag = "v${version}";
+      hash = "sha256-WDGYS2W0FCIDoayafzUjcwTAW+v2jxfJo54kaM6ymCE=";
+    };
+    ldflags = [
+      "-X main.version=${version}"
+      "-X main.buildSource=nix"
+    ];
+  });
 }
